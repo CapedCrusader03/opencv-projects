9628ac5d79f93c8a511bf7096696bf5d7a3dbe74
Kshitij <kshitijkumar3@gmail.com>
2020-07-17 11:07:37 +0530

    Added the comments

--- LeftMouseCallback.py
+++ LeftMouseCallback.py
@@ -1,21 +1,22 @@
+#importing the necessary packages
 import cv2
 import numpy as np
 
-#events = [i for i in dir(cv2) if 'EVENT' in i]
-#print(events)
-
+#defining a click event for the left mouse button
 def click_event(event,x,y,flags,param):
-    if event == cv2.EVENT_LBUTTONDOWN:
+    if event == cv2.EVENT_LBUTTONDOWN:  #Left mouse button event
         print(x, ', ',y)
         font = cv2.FONT_HERSHEY_SIMPLEX
         strXY = str(x) + ', ' + str(y)
-        cv2.putText(img, strXY, (x,y), font, 0.8, (255,255,0), 2)
+        cv2.putText(img, strXY, (x,y), font, 0.5, (255,255,0), 1, cv2.LINE_AA)
         cv2.imshow('image',img)
 
-img = np.zeros((512,512,3), np.uint8)
+#Creating a custom image using numpy.
+img = np.zeros((512,512,3), np.uint8)  #This creates a black image of 512x512 dimensions
 cv2.imshow('image', img)
 
+#Calling the click event
 cv2.setMouseCallback('image', click_event)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.waitKey(0) #This makes the frame appear for indefinite time.
+cv2.destroyAllWindows() #This destroys all the windows.
