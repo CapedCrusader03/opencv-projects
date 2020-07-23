253ade075b1d145fc7c93076986630182d39a7b0
Kshitij <kshitijkumar3@gmail.com>
2020-07-23 20:13:59 +0530

    Added the file

---
+++ SmoothingAndBlurring.py
@@ -0,0 +1,22 @@
+#Importing the necessary libraries
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('LM10.jpg')
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) #Conversion to a grayscale image
+
+kernel = np.ones((5,5), np.float32)/25 #Custom 5 x 5 kernel
+dst = cv2.filter2D(img, -1, kernel) #Convolves this kernel with the image 'img'
+blur = cv2.blur(img, (5,5)) #Average blurring
+gaussian_blur = cv2.GaussianBlur(img, (5,5), 0) #Gussian blurring
+
+titles = ['image', '2D Convolutions', 'Blur', 'GaussianBlur']
+images = [img,dst, blur, gaussian_blur]
+
+for i in range(4):
+    plt.subplot(2, 2, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
