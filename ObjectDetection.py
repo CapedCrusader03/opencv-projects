9d04dad95128ace677b108e20371cc9d120cbd93
Kshitij <kshitijkumar3@gmail.com>
2020-07-30 22:04:27 +0530

    Added the comments in ObjectDetection

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -1,26 +1,26 @@
+#Import the necessary libraries
 import cv2
-import numpy as np
 
-cap = cv2.VideoCapture('funny_dog.mp4')
+cap = cv2.VideoCapture('funny_dog.mp4') #Reading the video
 
-ret,frame1 = cap.read()
+ret,frame1 = cap.read() #read the frames of the video
 ret,frame2 = cap.read()
 
 while cap.isOpened():
-    diff = cv2.absdiff(frame1,frame2)
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-    dilated = cv2.dilate(thresh, None, iterations=3)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    diff = cv2.absdiff(frame1,frame2) #Find the difference between the two frames
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY) #Convert the frames to Grayscale
+    blur = cv2.GaussianBlur(gray, (5,5), 0) #Applying Gaussian blur
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY) #Adding threshold
+    dilated = cv2.dilate(thresh, None, iterations=3) #Dilating
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #Finding the contours
 
     for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
+        (x, y, w, h) = cv2.boundingRect(contour) #Get the x,y, width, height of the contour
 
         if cv2.contourArea(contour) < 10000:
-            continue
-        cv2.rectangle(frame1, (x,y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame1, "Status{}".format('Movement'), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3)
+            continue #if the area enclosed by th contour is less than 10000, then don't do anything
+        cv2.rectangle(frame1, (x,y), (x+w, y+h), (0, 255, 0), 2) #Else draw a rectangle around the contour
+        cv2.putText(frame1, "Status{}".format('Moving'), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3) #if the object is moving, then show the status as moving
 
     #cv2.drawContours(frame1, contours, -1, (0,255,0), 2)
 
@@ -28,7 +28,7 @@ while cap.isOpened():
     frame1 = frame2
     ret, frame2 = cap.read()
 
-    if cv2.waitKey(20) == 27:
+    if cv2.waitKey(20) == 27: #If the Esc key is pressed, then close the window
         break
 
 cv2.destroyAllWindows()
